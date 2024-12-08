2e5c35990c77f699653dc50d6a12b1400c35890e
Natalia <natalia.shchrbakova@gmail.com>
2024-12-08 19:49:41 +0300

    Добавление файла main.py
    Signed-off-by: Natalia <natalia.shchrbakova@gmail.com>

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import *
+t = Turtle()
+t.fillcolor("blue")
+t.screen.setup(800, 800)
+t.up()
+t.goto(-450, 0)
+t.down()
+t.setheading(270)
+for i in range(5):
+    t.circle(50, 180)
+    t.begin_fill() #Для закрашивания фигуры в модуле turtl
+    t.circle(-50, 180)
+    t.end_fill()
+t.screen.exitonclick()
+t.screen.mainloop()
